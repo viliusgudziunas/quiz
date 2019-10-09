cec7bf42226da61181680d58f2e2084f48f22557
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2019-10-09 02:47:14 +0100

    Part 1 - Postgres Setup

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # Instantiate the app
 app = Flask(__name__)
@@ -9,6 +10,22 @@ api = Api(app)
 # Set config
 app_settings = os.getenv("APP_SETTINGS")
 app.config.from_object(app_settings)
+
+# Instantiate the db
+db = SQLAlchemy(app)
+
+
+# Model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class UsersPing(Resource):
