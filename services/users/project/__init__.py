50d845bf3e87da4963f36d696f1c5ba012687af7
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2019-10-09 03:56:06 +0100

    Part 1 - Flask Blueprints

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,39 +1,23 @@
 import os
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# Instantiate the app
-app = Flask(__name__)
-api = Api(app)
-
-# Set config
-app_settings = os.getenv("APP_SETTINGS")
-app.config.from_object(app_settings)
-
-# Instantiate the db
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-# Model
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app(script_info=None):
+    app = Flask(__name__)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
 
+    db.init_app(app)
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-            "status": "success",
-            "message": "pong!"
-        }
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
 
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app, "db": db}
 
-api.add_resource(UsersPing, "/users/ping")
+    return app
