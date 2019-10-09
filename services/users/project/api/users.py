0898151ff20ee68f492e38004694079de9ca3672
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2019-10-09 08:06:42 +0100

    WIP: Part 1 - RESTful Routes

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,5 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restful import Resource, Api
+from sqlalchemy import exc
+from project import db
+from project.api.models import User
 
 users_blueprint = Blueprint("users", __name__)
 api = Api(users_blueprint)
@@ -13,4 +16,32 @@ class UsersPing(Resource):
         }
 
 
+class UsersList(Resource):
+    def post(self):
+        post_data = request.get_json()
+        response_object = {
+            "status": "fail",
+            "message": "Invalid payload."
+        }
+        if not post_data:
+            return response_object, 400
+        username = post_data.get("username")
+        email = post_data.get("email")
+        try:
+            user = User.query.filter_by(email=email).first()
+            if not user:
+                db.session.add(User(username=username, email=email))
+                db.session.commit()
+                response_object["status"] = "success"
+                response_object["message"] = f"{email} was added!"
+                return response_object, 201
+            else:
+                response_object["message"] = "Sorry. That email already exists."
+                return response_object, 400
+        except exc.IntegrityError:
+            db.session.rollback()
+            return response_object, 400
+
+
 api.add_resource(UsersPing, "/users/ping")
+api.add_resource(UsersList, "/users")
