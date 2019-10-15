189877c5ad78a4e019d293f21b3460d35ba7c898
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2019-10-15 03:01:36 +0100

    Part 2 - React Forms

---
+++ services/client/src/components/AddUser.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const AddUser = ({ setUserAdded }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  const addUser = e => {
+    e.preventDefault();
+    const data = { username, email };
+    axios
+      .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then(res => {
+        setUserAdded(true);
+        setUsername("");
+        setEmail("");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <form onSubmit={addUser}>
+      <div className="field">
+        <input
+          type="text"
+          className="input is-large"
+          name="username"
+          placeholder="Enter a username"
+          required
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="field">
+        <input
+          type="email"
+          className="input is-large"
+          name="email"
+          placeholder="Enter an email address"
+          required
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
+      <input
+        name="submit"
+        type="submit"
+        className="button is-primary is-large is-fullwidth"
+        value="Submit"
+      />
+    </form>
+  );
+};
+
+export default AddUser;
--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import UsersList from "./components/UsersList";
+import AddUser from "./components/AddUser";
 
 const App = () => {
   const [users, setUsers] = useState([]);
 
+  const [userAdded, setUserAdded] = useState(false);
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
@@ -15,19 +17,23 @@ const App = () => {
       .catch(err => {
         console.log(err);
       });
-  }, [setUsers]);
+    setUserAdded(false);
+  }, [setUsers, userAdded]);
 
   return (
     <section className="section">
       <div className="container">
         <div className="columns">
-          <column className="is-one-third">
+          <div className="is-one-third">
             <br />
             <h1 className="title is-1">All Users</h1>
             <hr />
             <br />
+            <AddUser setUserAdded={setUserAdded} />
+            <hr />
+            <br />
             <UsersList users={users} />
-          </column>
+          </div>
         </div>
       </div>
     </section>
