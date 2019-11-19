375c132dfdac9fe37d166970e8295044587846e8
Vilius Gudziunas <vilius.gudziunas@yahoo.com>
2019-11-19 23:54:46 +0000

    Part 4 - End-to-End Test Setup

--- test.sh
+++ test.sh
@@ -21,7 +21,7 @@ docker-compose down
 # run e2e tests
 docker-compose -f docker-compose-prod.yml up -d --build
 docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
-./node_modules/.bin/cypress run --config baseUrl=http://192.168.99.102
+./node_modules/.bin/cypress run --config baseUrl=http://localhost
 inspect $? e2e
 docker-compose -f docker-compose-prod.yml down
 
